42efa3d3079c5aaeac331caff43eb677e7a76129
Hardhik Reddy <96832540+hardhik2002@users.noreply.github.com>
2022-05-04 21:17:55 +0530

    buildit extarnal exam

--- buildit exam.py
+++ buildit exam.py
@@ -1,3 +1,26 @@
+"""
+A wrestling bout is a physical competition, between two teams namely X-team and Y-person, 
+who attempt to gain and maintain a superior position. The team contains n persons with various powers p1,p2,p3,p4…..pn. 
+The X-team is going to win only if you find a combination of two people’s power in x team is equal to Y person power. 
+The team is busy with practice, please help them to find whether the team-x is won or not. If won then print “YES” otherwise print “NO”
+INPUT:
+First line contain x-team input values.
+Second line contain Y-team player input value.
+OUTPUT:
+If X-team won then print “YES” otherwise print “NO”
+Sample input 1:
+23 45 67 89 12
+57
+Sample output 1:
+YES
+Sample input 2:
+23 45 67 89 12
+56
+Sample output 2:
+NO
+
+"""
+
 powers =[int(i) for i in input().split()]
 y_pow = int(input())
 
