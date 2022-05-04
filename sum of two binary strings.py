2270048c564b9219687c07da5bfb38a4d8e43061
Hardhik Reddy <96832540+hardhik2002@users.noreply.github.com>
2022-05-04 21:20:36 +0530

    external

--- sum of two binary strings.py
+++ sum of two binary strings.py
@@ -1,4 +1,30 @@
 # program-3 in buildit lab external
+"""
+Given two binary strings a and b, return their sum as a binary string.
+Input:
+Input contains two lines 
+First binary number
+Secondary binary number
+Output:
+Output contains one line
+Sum of binary numbers
+Sample input 1:
+10
+1
+Sample Output 1:
+11
+Sample input 2:
+101
+10
+Sample output 2:
+111
+Sample input 3:
+1010
+1011
+Sample output 3:
+10101
+
+"""
 b1=input()
 b2=input()
 b1,b2=int(b1),int(b2)
