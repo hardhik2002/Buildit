c25713bc7172e1e18b966c6023c27d21ecb83b53
Hardhik Reddy <96832540+hardhik2002@users.noreply.github.com>
2022-05-04 21:18:54 +0530

    external

--- program 2.py
+++ program 2.py
@@ -1,3 +1,25 @@
+"""
+You’re given strings jewels representing the types of stones that are jewels, and stones you have. 
+Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+Letters are case sensitive, so “a” is considered a different type of stone from “A”.
+Input:
+Input of two lines
+First line is a string
+Second line is also a string
+Output:
+Output single line – displays a number
+Sample input 1:
+a
+abc
+Sample Output 1:
+1
+Sample input 2:
+aA
+aAAbbbb
+Sample output 2:
+3
+
+"""
 str1=input()
 str2=input()
 count=0
