96519c001822374c6e6a0b05295f238258a23171
Hardhik Reddy <96832540+hardhik2002@users.noreply.github.com>
2022-05-04 21:19:52 +0530

    external

--- program-4.py
+++ program-4.py
@@ -1,3 +1,27 @@
+"""
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. 
+You are given an array of string sentences, where each sentences[i] represents a single sentence.
+Return the maximum number of words that appear in a single sentence.
+Input:
+Input contains n+1 lines
+First line the number of sentences in the list
+Next n lines contains n sentences
+Output:
+Output is a number
+Sample input 1 :
+3
+alice and bob love IARE
+i think so too
+it is a test
+Sample output 1:
+5
+Sample input 2:
+1
+This is so test
+Sample Output 2:
+4
+
+"""
 lst=[]
 for i in range(int(input())):
     sentence=[i for i in input().split()]
